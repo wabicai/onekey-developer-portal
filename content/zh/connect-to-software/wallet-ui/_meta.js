1bddce508a5a03bd118a2dd629e72ff147c2ff9b
wabicai <kaisheng.cai@onekey.so>
2025-12-01 13:11:41 +0800

    fix: aptos

--- content/zh/connect-to-software/wallet-ui/_meta.js
+++ content/zh/connect-to-software/wallet-ui/_meta.js
@@ -2,7 +2,8 @@ export default {
   'wallet-kits': '概览',
   rainbowkit: 'RainbowKit',
   web3modal: 'Web3Modal',
-  'web3-onboard': 'Web3-Onboard'
+  'web3-onboard': 'Web3-Onboard',
+  'aptos-wallet-adapter': 'Aptos Wallet Adapter'
 }
 
 
